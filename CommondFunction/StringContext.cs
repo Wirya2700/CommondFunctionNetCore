6543e932b6c39c5ef3aa014a4b5ffeb506497cc2
Wirya Almustofa <wirya@qbit.co.id>
2021-08-08 09:58:15 +0700

    Add project files.

---
+++ CommondFunction/StringContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CommondFunction
+{
+    public static class StringContext
+    {
+        public static bool IsNotNullOrWhiteSpace(this string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return false;
+            return true;
+        }
+    }
+}
---
+++ CommondFunctionTests/StringContextTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CommondFunction;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommondFunction.Tests
+{
+    [TestClass()]
+    public class StringContextTests
+    {
+        [TestMethod()]
+        public void IsNotNullOrWhiteSpaceTest()
+        {
+            string cek1 = string.Empty;
+            string cek2 = "        ";
+            string cek3 = null;
+
+            bool resultCek1 = cek1.IsNotNullOrWhiteSpace();
+            bool resultCek2 = cek2.IsNotNullOrWhiteSpace();
+            bool resultCek3 = cek3.IsNotNullOrWhiteSpace();
+
+            Assert.IsTrue(resultCek1 == false);
+            Assert.IsTrue(resultCek2 == false);
+            Assert.IsTrue(resultCek3 == false);
+        }
+    }
+}
